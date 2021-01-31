8fccc53dd8625a39c5dddde415f99a7fa747cea3
Ming Huo <mhuo0909@gmail.com>
2021-01-31 14:53:33 -0500

    add base website

---
+++ head.js
@@ -0,0 +1,6 @@
+document.write(
+'   <meta name="author" content="Ming Huo">'+
+'	<link rel="stylesheet" type="text/css" href="css/theme.css" />'+
+'	<link rel="shortcut icon" href="img/ico.png"/>'
+);
+
